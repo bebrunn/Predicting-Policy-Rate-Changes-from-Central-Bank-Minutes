d689f3a9828d18d96b3c5e85aa61bcc21202fca8
Bernhard Brunner <bbrunner@skirit.metacentrum.cz>
2024-10-04 13:56:23 +0200

    Fix predict_sentiment.py file

--- code/predict_sentiment.py
+++ code/predict_sentiment.py
@@ -11,7 +11,7 @@ import spacy
 import numpy as np
 
 # Import data set and model for inference
-from cbminutes_dataset import CBMinutesDataset
+from cbminutes_dataset import Vocabulary, CBMinutesDataset
 from sentiment_analysis import parser, Model
 
 # Create argsparser to adjust arguments in shell.
@@ -44,7 +44,7 @@ def main(args):
     backbone = transformers.AutoModel.from_pretrained(args.backbone)
     dataset = CBMinutesDataset("../data")
     model = Model(args, backbone, dataset.train)
-    model.load_state_dict(torch.load(args.model_weights))
+    model.load_state_dict(torch.load(args.model_weights, weights_only=True))
     model.eval()
 
     # Load new data
@@ -59,7 +59,7 @@ def main(args):
 
     # Loop over documents and classify their sentences
     for doc_name, sentences in documents.items():
-
+        # Classify all documents in batches of 16.
         for i in range(0, len(sentences), 16):
 
             # Tokenize the each sentence in a document
@@ -74,14 +74,18 @@ def main(args):
                 predictions = model(input_ids, attention_mask)
                 sentence_predictions = torch.argmax(predictions, dim=1).cpu().numpy()
 
-        # Determine sentiment of overall minute by majority vote
+        # Determine sentiment of overall minute by majority vote.
+        # FIXME: I can determine the degree of hawking and dovishness by call np.mean(sentence_predictions) - 1
+        # if dovish=0, neutral=1, and hawkish=1.
         overall_sentiment = np.bincount(sentence_predictions).argmax()
 
         # Store overall sentiment for each document
-        document_sentiments[doc_name] = overall_sentiment
+        document_sentiments[doc_name] = dataset.train.label_vocab.string(int(overall_sentiment))
 
     # Save the predicition
-    with open("sentiment_predictions.tsv", "w", encoding="utf-8") as file:
+    pred_directory = "../predictions"
+    os.makedirs(pred_directory, exist_ok=True)
+    with open(os.path.join(pred_directory, "sentiment_predictions.tsv"), "w", encoding="utf-8") as file:
         for doc_name, sentiment in document_sentiments.items():
             file.write(f"{doc_name}\t{sentiment}\n")
 
